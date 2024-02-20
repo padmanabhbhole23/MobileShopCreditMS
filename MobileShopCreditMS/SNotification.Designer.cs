5b796ea36a9f3e127c86ff21834b4a2ae43b9275
padmanabhbhole23 <padmanabhbhole23@gmail.com>
2024-02-20 14:38:48 +0530

    20/2/23

--- MobileShopCreditMS/SNotification.Designer.cs
+++ MobileShopCreditMS/SNotification.Designer.cs
@@ -28,12 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "SNotification";
+            label1 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(87, 128);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(165, 129);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(208, 137);
+            textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(433, 182);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 2;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // SNotification
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "SNotification";
+            Text = "SNotification";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Button button1;
     }
 }
--- MobileShopCreditMS/SNotification.cs
+++ MobileShopCreditMS/SNotification.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +17,22 @@ namespace MobileShopCreditMS
         public SNotification()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                WebClient cl = new WebClient();
+                Stream s = cl.OpenRead(string.Format("http://api.clickatell.com/http/sendmsg?user=unclepaani1708363628&password=uncleJIpaani2&api_id=[d6f4978ee1c7430cb3d0bea54906c651]&to=(0)&text=(1)",9860017436,textBox1.Text));
+                StreamReader reader = new StreamReader(s);  
+                string result= reader.ReadToEnd();
+                MessageBox.Show(result,"Message",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message,"Message",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
         }
     }
 }
